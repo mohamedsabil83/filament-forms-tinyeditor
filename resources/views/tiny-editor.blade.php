3c394b680d58157ccd17df10e458c617faffbfaa
MohamedSabil83 <me@mohamedsabil83.com>
2022-04-01 15:32:05 +0300

    Improve content preview on dark mode

--- resources/views/tiny-editor.blade.php
+++ resources/views/tiny-editor.blade.php
@@ -10,63 +10,63 @@
     <div
         x-data="{ state: $wire.entangle('{{ $getStatePath() }}'), initialized: false }"
         x-init="(() => {
-                window.addEventListener('DOMContentLoaded', () => initTinymce())
-                $nextTick(() => initTinymce())
-                const initTinymce = () => {
-                    if (window.tinymce !== undefined && initialized === false) {
-                        tinymce.init({
-                            target: $refs.tinymce,
-                            language: '{{ $getInterfaceLanguage() }}',
-                            skin: typeof theme != 'undefined' ? theme : 'light',
-                            content_css: this.skin === 'dark' ? 'dark' : '',
-                            max_height: {{ $getHeight() }},
-                            menubar: {{ $getShowMenuBar() ? 'true' : 'false' }},
-                            plugins: ['{{ $getPlugins() }}'],
-                            toolbar: '{{ $getToolbar() }}',
-                            toolbar_mode: 'sliding',
-                            branding: false,
-                            images_upload_handler: (blobInfo, success, failure, progress) => {
-                                if (!blobInfo.blob()) return
+            window.addEventListener('DOMContentLoaded', () => initTinymce())
+            $nextTick(() => initTinymce())
+            const initTinymce = () => {
+                if (window.tinymce !== undefined && initialized === false) {
+                    tinymce.init({
+                        target: $refs.tinymce,
+                        language: '{{ $getInterfaceLanguage() }}',
+                        skin: typeof theme != 'undefined' ? theme : 'light',
+                        content_css: this.skin === 'dark' ? 'dark' : '',
+                        max_height: {{ $getHeight() }},
+                        menubar: {{ $getShowMenuBar() ? 'true' : 'false' }},
+                        plugins: ['{{ $getPlugins() }}'],
+                        toolbar: '{{ $getToolbar() }}',
+                        toolbar_mode: 'sliding',
+                        branding: false,
+                        images_upload_handler: (blobInfo, success, failure, progress) => {
+                            if (!blobInfo.blob()) return
 
-                                $wire.upload(`componentFileAttachments.{{ $getStatePath() }}`, blobInfo.blob(), () => {
-                                    $wire.getComponentFileAttachmentUrl('{{ $getStatePath() }}').then((url) => {
-                                        if (!url) {
-                                            failure('{{ __('Error uploading file') }}')
-                                            return
-                                        }
-                                        success(url)
-                                    })
-                                })
-                            },
-                            automatic_uploads: true,
-                            setup: function (editor) {
-                                editor.on('blur', function (e) {
-                                    state = editor.getContent()
-                                })
-
-                                editor.on('init', function (e) {
-                                    if (state != null) {
-                                        editor.setContent(state)
+                            $wire.upload(`componentFileAttachments.{{ $getStatePath() }}`, blobInfo.blob(), () => {
+                                $wire.getComponentFileAttachmentUrl('{{ $getStatePath() }}').then((url) => {
+                                    if (!url) {
+                                        failure('{{ __('Error uploading file') }}')
+                                        return
                                     }
+                                    success(url)
                                 })
+                            })
+                        },
+                        automatic_uploads: true,
+                        setup: function(editor) {
+                            editor.on('blur', function(e) {
+                                state = editor.getContent()
+                            })
 
-                                function putCursorToEnd() {
-                                    editor.selection.select(editor.getBody(), true);
-                                    editor.selection.collapse(false);
+                            editor.on('init', function(e) {
+                                if (state != null) {
+                                    editor.setContent(state)
                                 }
+                            })
 
-                                $watch('state', function (newstate) {
-                                    if (newstate !== editor.getContent()) {
-                                        editor.resetContent(newstate || '');
-                                        putCursorToEnd();
-                                    }
-                                });
+                            function putCursorToEnd() {
+                                editor.selection.select(editor.getBody(), true);
+                                editor.selection.collapse(false);
                             }
-                        })
-                        initialized = true
-                    }
+
+                            $watch('state', function(newstate) {
+                                if (newstate !== editor.getContent()) {
+                                    editor.resetContent(newstate || '');
+                                    putCursorToEnd();
+                                }
+                            });
+                        }
+                    })
+                    initialized = true
                 }
-            })()"
+            }
+        })()"
         x-cloak
         wire:ignore
     >
@@ -79,7 +79,7 @@
         @else
             <div
                 x-html="state"
-                class="max-w-none p-3 prose border border-gray-300 rounded shadow-sm"
+                class="prose dark:prose-invert block w-full max-w-none rounded-lg border border-gray-300 bg-white p-3 opacity-70 shadow-sm transition duration-75 dark:border-gray-600 dark:bg-gray-700"
             ></div>
         @endunless
     </div>
